e7e18d4464475c03e69aaa3244a87ce9effcd136
Straka <vladimir.strak@gmail.com>
2025-09-29 19:09:11 +0200

    form style input with basic poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
+import * as React from "react";
 
 interface Todo {
   id: number,
@@ -11,29 +12,38 @@ function App() {
   const [todos, setTodos] = useState<Todo[]>([])
   const [newTodo, setNewTodo] = useState('')
 
-  function handleNewTodo(newTodo: string) {
+  // todo validace
+  function handleNewTodo(event: React.SyntheticEvent<HTMLFormElement>) {
+    event.preventDefault()
     const id = todos.length + 1
     setTodos([...todos, { id, title: newTodo}])
+    setNewTodo('')
   }
 
   return (
     <>
       <header>
-        <input
-          className="border-2 rounded-md mr-6 p-0.5"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-        />
-        <button className="border-1 rounded-md p-1" onClick={() => handleNewTodo(newTodo)}>Add</button>
+        <form onSubmit={handleNewTodo}>
+          {/*// todo input komponenta*/}
+          <label>Todo name</label>
+          <input
+            className="border-2 rounded-md mr-6 p-0.5"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          {/*// todo button komponenta s viditelnym disabled*/}
+          <button type="submit" className="border-1 rounded-md p-1">Add</button>
+        </form>
+
       </header>
-      <main>
+      <main className="border-2 rounded-md p-6 border-red-300">
+        {/*todo todos komponenta*/}
         <ul>
           {todos.map((todo) => (
             <li key={todo.id}>{todo.title}</li>
           ))}
         </ul>
       </main>
-
     </>
   )
 }
